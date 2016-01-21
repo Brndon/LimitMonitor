93a080da742201b1f9972c067b60cc28d84dcf55
trobiv <trobiv@gmail.com>
2016-01-21 10:17:54 -0800

    Update limitMaster.py

--- limitMaster.py
+++ limitMaster.py
@@ -2,27 +2,19 @@
 
 import boto3
 import ConfigParser
-
-#Sets up the process to read the config file
-config = ConfigParser.ConfigParser()
-config_dict = {}
-regions = []
-config.read("accounts.config")
-options = config.options('Settings')
-for option in options:
-	config_dict[option] = config.get('Settings', option)
-
-ids = config_dict['id'].split(',')
-
-lambda_client = boto3.client('lambda', region_name='us-east-1')
+import json
 
 def lambda_handler(event, context):
-	for id in ids:
+	lambda_client = boto3.client('lambda', region_name=event['Region'])
+	for id in event['AccountList']:
 		print "do stuff here with AWS account "+id+"\n"
+		event['AccountId'] = id
+		eventbytes=json.dumps(event)
 		response = lambda_client.invoke(
-			FunctionName='LimitMonkeyEE',
+			FunctionName=event['ChildLambda'],
 			InvocationType='Event',
 			ClientContext='string',
-			Payload=id 
+			Payload=eventbytes
 		)
+		print response
 	return;
